98cd7b3b9c058a68aed5b1099e0835e93fce7202
louishan42 <thihanhtun@my.jcu.edu.au>
2026-03-04 22:59:58 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -103,5 +103,8 @@ await ensureRooms();
 await ensureDoctorSchedules();
 await ensureSystemUsers();
 
-const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
